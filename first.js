db00c24798f01ff6204610a78158ff29d1f4af66
Gatpri <saugatkapri@gmail.com>
2026-05-07 13:18:55 +0545

    Practiced Variables in JS

---
+++ first.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!");
+// This is a simple JavaScript file that prints "Hello, World!" to the console.
+// We should enter the command "node first.js" in the terminal to run this file.
+
+var a=10;
+console.log(a);
+// This code declares a variable 'a' and assigns it the value of 10, then prints the value of 'a' to the console.   
+var a=20;
+console.log(a);
+// This code reassigns the variable 'a' to the value of 20 and prints it to the console.  
+console.log("The value of a is:" + a);
+var b=5;
+console.log("The value of b is:" +b);
+// This code declares a variable 'b' and assigns it the value of 5, then prints the value of 'b' to the console.
+var sum=a+b;
+console.log("The sum of a and b is:" + sum);
