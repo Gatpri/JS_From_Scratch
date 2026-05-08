7f2670430a46dfc0f0f1bfd945b50db268e2b15e
Gatpri <saugatkapri@gmail.com>
2026-05-08 18:35:35 +0545

    WHILE and DO-WHILE loops

---
+++ while_loops.js
@@ -0,0 +1,43 @@
+/* In this we are going to learn about while loops in javascript. A while loop is a control flow statement that allows code to be executed repeatedly based on a given condition. The syntax of a while loop is as follows:*/
+
+/*while (condition) {
+    // code to be executed
+}*/
+
+/*******************************************************/
+/*
+const rl = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter a number: ", (number) => {
+
+
+    console.log("The number you entered is:", number);
+    let i = 0;
+    while (i < number) {
+        console.log("Iteration number:", i);
+        i++;
+    }
+    rl.close();   
+});
+*/
+
+
+//Now using do while loop
+
+const rl2 = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl2.question("Enter a number: ", (number) => {
+    console.log("The number you entered is:", number);
+    let i = 0;  
+    do {
+        console.log("Iteration number:", i);
+        i++;
+    } while (i < number);
+    rl2.close();
+});
