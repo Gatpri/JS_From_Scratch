06551104949dd8e416b5b198fea83d2f7b9ca95c
Gatpri <saugatkapri@gmail.com>
2026-05-08 22:24:50 +0545

    About Funnctions in JS

---
+++ functions.js
@@ -0,0 +1,31 @@
+function onePlusAvg(x,y){
+    return 1+(x+y)/2
+}
+
+let a=1;
+let b=2;
+let c=3;
+/*
+console.log("One plus average of a and b is", 1+(a+b)/2);
+console.log("One plus average of b and c is", 1 +(b+c)/2);
+console.log("One plus average of a and c is is", 1 +(b+c)/2);
+*/
+console.log("One plus average of a and b is", onePlusAvg(a,b));
+console.log("One plus average of b and c is", onePlusAvg(b,c));
+console.log("One plus average of a and c is", onePlusAvg(a,c));
+
+
+
+
+const sum = (p,q)=>{
+    return p+q;
+};
+
+console.log(sum(16,4));
+
+
+const hello=()=>{
+    console.log("hey i had made the function for the practice section ");
+}
+
+hello();
