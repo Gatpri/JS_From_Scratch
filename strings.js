132f35fd04a89f2fda8a0cbef63a16a48324cbb1
Gatpri <saugatkapri@gmail.com>
2026-05-10 13:01:52 +0545

    About Strings IN JS

---
+++ strings.js
@@ -0,0 +1,23 @@
+let name ="saugat";
+
+console.log(name.length);
+
+let friend = "harry";
+console.log(friend);
+
+let myFriend ="Abhaya"
+console.log(myFriend);
+console.log("printing using length",myFriend.length);
+console.log("printing word starting at 0",myFriend[0]);
+console.log("printing word starting at 1",myFriend[1]);
+console.log("printing word starting at 2",myFriend[2]);
+console.log("printing word starting at 3",myFriend[3]);
+console.log("printing word starting at 4",myFriend[4]);
+console.log("printing word starting at 5",myFriend[5]);
+
+
+
+let boy1 = "saugat";
+let boy2 = "Abhaya";
+let sentence = `${boy1} is a friend of ${boy2}`;
+console.log(sentence);
